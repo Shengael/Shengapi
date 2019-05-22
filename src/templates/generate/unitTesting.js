87ead91588221bed1d543a6420b3fdf65ca0685d
Luis Valdez <valdez.jds.16@gmail.com>
2019-05-22 22:43:06 +0200

    adding package and unitTesting template

--- src/templates/generate/unitTesting.js
+++ src/templates/generate/unitTesting.js
@@ -1,1 +1,56 @@
-//unitTesting $Model$Test
+'use strict';
+const dotenv = require('dotenv');
+const test_connection = require('../../config').mongo_connection;
+const mongoose = require('mongoose');
+
+
+dotenv.config();
+let $controller$;
+
+const expect = require('chai').expect;
+
+
+before(async function() {
+    await test_connection('test');
+    $controller$ = require('./User.controller');
+    //await mongoose.connection.collections.User.drop();
+});
+
+describe('$Model$ Controller', () => {
+    describe('#create()', () => {
+        it('should return new $Model$', async () => {
+            const $model$ = await $controller$.create();
+            expect($model$).to.not.be.undefined;
+
+        });
+    });
+    describe('#getAll()', () => {
+        it('should return $Model$ array', async () => {
+            const $model$s = await $controller$.getAll();
+            expect($model$s).to.be.an('array');
+            expect($model$s).to.have.length(1);
+        });
+    });
+
+    describe('#getById()', () => {
+        it('should return an $Model$', async () => {
+            const $model$ = await $controller$.getById(1);
+            expect($model$).to.not.be.undefined;
+        });
+
+        it('should return undefined', async () => {
+            const users = await $controller$.getById(2);
+            expect(users).to.be.null;
+        });
+    });
+
+    describe('#update()', () => {
+
+    });
+
+} );
+
+after(async function() {
+    await mongoose.connection.collections.User.drop();
+    mongoose.connection.close();
+});
